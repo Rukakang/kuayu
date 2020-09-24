e9758ce39657f1e22de007534cd7fd517df22ed7
903958734@qq.com <903958734@qq.com>
2020-09-24 23:51:40 +0800

    跨域、CORS、JSONP

---
+++ heike-com/public/heike.js
@@ -0,0 +1,55 @@
+function jsonp(url){
+    return new Promise((resolve,reject)=>{
+        const random = 'heikeJSONPCallbackName'+Math.random()
+        window[random] =(data)=>{
+            resolve(data);
+        };
+        const script = document.createElement('script')
+        script.src = `${url}?callback=${random}`;
+        script.onload=()=>{
+            script.remove;
+        };
+        script.onerror=()=>{
+            reject();
+        };
+        document.body.appendChild(script);
+    })
+}
+
+jsonp('http://qq.com:8888/friends.js')
+.then((data)=>{
+    console.log(data);
+})
+
+
+function ajax(method, url) {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.open(method, url);
+      request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+          if (request.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(request);
+          }
+        }
+      };
+      request.send();
+    });
+  }
+  ajax("get", "http://qq.com:8888/friends.json").then(response => {
+    console.log("这是 AJAX");
+    console.log(response);
+  });
+
+/* const request = new XMLHttpRequest();
+request.open('GET','http://qq.com:8888/friends.json');
+request.onreadystatechange=()=>{
+    if(request.readyState ===4 && request.status ===200){
+        console.log(request.response);
+    }
+}
+request.send();*/
+
+
